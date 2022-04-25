c9c97288684f8c7aef06b57ea7f81b93f6361094
KaydanYard <86071974+KaydanYard@users.noreply.github.com>
2022-04-25 09:06:00 -0600

    fixed the style clashes.  Bootstrap is now removed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,14 @@ import { HomeComponent } from './components/home/home.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+
 
 @NgModule({
   declarations: [
@@ -30,14 +33,16 @@ import { MatTableModule } from '@angular/material/table';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    MatButtonModule,
     MatCardModule,
+    MatCheckboxModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule,
     MatRadioModule,
     MatSelectModule,
-    MatButtonModule,
     MatTableModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
