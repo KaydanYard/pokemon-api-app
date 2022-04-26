0a99bfba1db2d4492d72cb0756348c511cb2d444
KaydanYard <86071974+KaydanYard@users.noreply.github.com>
2022-04-26 11:00:44 -0600

    still working on styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { PokefindComponent } from './components/pokefind/pokefind.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 import { AuthGuard } from './guard/auth.guard';
 
+import { HomeComponent } from './components/home/home.component';
+import { PokefindComponent } from './components/pokefind/pokefind.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignupComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'verify-email-address', component: VerifyEmailComponent },
+  { path: 'pokefind', component: PokefindComponent },
+  { path: '**', component: HomeComponent },
+
 ];
 
 @NgModule({
